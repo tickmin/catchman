6fbbc95f0fffb00fa3cf6a037ab821a7133ab3fc
tickmin <60147212+tickmin@users.noreply.github.com>
2020-01-21 17:52:32 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def get_authenticate(usuario,senha):
     return "<script>window.location.replace('https://sig.ifc.edu.br/')</script>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, port=5000)
